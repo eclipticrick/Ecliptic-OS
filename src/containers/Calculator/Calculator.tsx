5f208b255c876a966bdc36cb6119b878a2a7faac
eclipticrick <eclipticrick@gmail.com>
2018-12-25 16:30:26 +0100

    - improved calculator functionality (can handle very large numbers in calculations now)

--- src/containers/Calculator/Calculator.tsx
+++ src/containers/Calculator/Calculator.tsx
@@ -4,14 +4,13 @@ import Window from '../../components/Window/Base/Window';
 import WindowMenu from './WindowMenu/WindowMenu';
 import * as classes from './Calculator.module.scss';
 import {Button, Grid, Tooltip} from '@material-ui/core';
-import * as classNames from 'classNames';
 
 interface ICalculatorState {
-    entry: number
-    store: number
+    entry: string
+    store: string
     operator: string
     lastChar: number | string
-    memory: number
+    memory: string
 }
 
 enum Clicked {
@@ -35,35 +34,55 @@ const menuTree = {
 
 export class Calculator extends React.Component<IDefaultApplicationWindowProps, ICalculatorState> {
     public state = {
-        entry: 0,
-        store: 0,
+        entry: '0',
+        store: null as string,
         operator: null as string,
         lastChar: null as number | string,
-        memory: 0
+        memory: null as string
     };
 
     public render() {
         const { windowInstance, application, selected } = this.props;
 
+        // todo: implement
         const menuItemClickedHandler = (menuItem: Clicked) => {
             console.log(menuItem, 'clicked')
         };
 
+        // todo: beautify code of this function and remove comments
         const handleButtonClick = (char: string | number) => {
 
-            const calculate = (n1: number, o: string, n2: number): number => {
-                if (o === '/' && n2 === 0) return +'NaN :)';
-                if (o === '/') return n1 / n2;
-                if (o === '*') return n1 * n2;
-                if (o === '+') return n1 + n2;
-                if (o === '-') return n1 - n2;
+            const trimNr = (nr: string) => {
+                if (typeof nr.split('.')[1] !== 'undefined') {
+                    while (nr.slice(-1) === '0') {
+                        nr = nr.slice(0, -1);
+                    }
+                }
+                if (nr.slice(-1) === '.') {
+                    return nr.slice(0, -1);
+                } else {
+                    return nr
+                }
             };
-            const calculatePercentage = (nr: number, o: string, perc: number): number => {
-                const amount = (nr * perc) / 100;
-                if (o === '/') return nr / amount;
-                if (o === '*') return nr * amount;
-                if (o === '+') return nr + amount;
-                if (o === '-') return nr - amount;
+
+            const calculate = (nr1: string, operator: string, nr2: string): string => {
+                nr1 = trimNr(nr1);
+                nr2 = trimNr(nr2);
+                if (operator === '/' && nr2 === '0') return 'Fatal Division by 0 error';
+                if (operator === '/') return (Number(nr1) / Number(nr2)).toString();
+                if (operator === '*') return (Number(nr1) * Number(nr2)).toString();
+                if (operator === '+') return (Number(nr1) + Number(nr2)).toString();
+                if (operator === '-') return (Number(nr1) - Number(nr2)).toString();
+            };
+            const calculatePercentage = (nr: string, operator: string, perc: string): string => {
+                nr = trimNr(nr);
+                perc = trimNr(perc);
+
+                const amount = (Number(nr) * Number(perc)) / 100;
+                if (operator === '/') return (Number(nr) / Number(amount)).toString();
+                if (operator === '*') return (Number(nr) * Number(amount)).toString();
+                if (operator === '+') return (Number(nr) + Number(amount)).toString();
+                if (operator === '-') return (Number(nr) - Number(amount)).toString();
             };
 
             const operators = ['/', '+', '*', '-'];
@@ -74,43 +93,126 @@ export class Calculator extends React.Component<IDefaultApplicationWindowProps, 
             this.setState((state: ICalculatorState) => {
 
                 if (typeof char === 'number') {
-                    newState.entry = (state.entry * 10) + char;
+
+                    // if the entry is not equal to 0
+                    if (state.entry !== '0') {
+
+                        // replace the entry when the number contains an e+ / e-
+                        if (state.entry.includes('e')) {
+                            newState.entry = String(char)
+                        } else if (state.entry.length >= 21) {
+                            // else if: max length of 21 chars has been reached (including the dot)
+
+                            // don't add anything to the entry
+                            newState.entry = state.entry
+                            // todo: play sound?
+                        } else {
+                            // else: add the number to the end of the entry
+                            newState.entry = state.entry += char
+                        }
+
+                    } else {
+
+                        // else: replace the 0 with the typed number
+                        newState.entry = String(char)
+                    }
                 } else if (char === '.') {
-                    console.log('is .');
+
+                    // the max has not been reached yet
+                    if (state.entry.length < 21) {
+
+                        // if there is an NO e in the entry
+                        if (!state.entry.includes('e')) {
+                            // if there is not dot yet
+                            if (!state.entry.includes('.')) {
+                                // add it
+                                newState.entry = state.entry += char
+
+                            } else if (state.entry.includes('.')) {
+                                // else if: there already is a dot and no e
+
+                                // don't add another dot
+                                newState.entry = state.entry
+                                // todo: play sound?
+                            }
+                        } else {
+                            // else: make it '0.'
+                            newState.entry = '0' + char
+                        }
+                    } else {
+
+                        // if the number contains an e
+                        if (state.entry.includes('e')) {
+                            // make it '0.'
+                            newState.entry = '0' + char
+                        } else {
+                            // don't add another dot
+                            newState.entry = state.entry
+                            // todo: play sound?
+                        }
+                    }
+
                 } else if (char === 'Backspace') {
-                    newState.entry = (state.entry - (state.entry % 10)) / 10;
+
+                    // if there is only one char left in the entry
+                    if (state.entry.length === 1) {
+
+                        // change it to 0
+                        newState.entry = '0'
+                    } else if (!state.entry.includes('e')) {
+                        // else if: the number does not contain an e+ / e-
+
+                        // cut the last char off
+                        newState.entry = state.entry.slice(0, -1)
+                    }
                 } else if (char === 'C') {
-                    newState.store = 0;
-                    newState.entry = 0;
+                    newState.store = null;
+                    newState.entry = '0';
                     newState.operator = null;
                 } else if (char === 'CE') {
-                    newState.entry = 0;
+                    newState.entry = '0';
                 } else if (operators.includes(char)) {
-                    newState.entry = 0;
+                    newState.entry = '0';
+
+                    // if no operator was specified before
                     if (!state.operator) {
-                        newState.store = state.entry;
+
+                        // trim the entry & move it to the store
+                        newState.store = trimNr(state.entry);
                     } else if (!operators.includes(String(state.lastChar))) {
+                        // else if the previous charachter was NOT an operator
+
+                        // calculate the new store with the [oldStore, operator, entry]
                         newState.store = calculate(state.store, state.operator, state.entry)
                     }
+
+                    // put the operator in the state
                     newState.operator = char;
                 } else if (calculations.includes(char)) {
                     if (char === '=') {
                         newState.operator = null;
-                        newState.store = 0;
+                        newState.store = null;
+
+                        // is it possible that operator === null? if so.. add another else if
+
                         if (!state.store) {
                             newState.entry = state.entry;
                         } else {
                             newState.entry = calculate(state.store, state.operator, state.entry);
                         }
                     } else if (char === 'sqrt') {
-                        newState.entry = Math.sqrt(state.entry)
+                        newState.entry = Math.sqrt(Number(trimNr(state.entry))).toString()
                     } else if (char === '1/x') {
-                        newState.entry = Math.pow(state.entry, -1)
+                        newState.entry = Math.pow(Number(trimNr(state.entry)), -1).toString()
                     } else if (char === '+/-') {
-                        newState.entry = state.entry >= 0 ? -Math.abs(state.entry) : Math.abs(state.entry);
+                        newState.entry = String(
+                            Number(trimNr(state.entry)) >= 0 ?
+                                -Math.abs(Number(trimNr(state.entry))) :
+                                Math.abs(Number(trimNr(state.entry)))
+                        );
                     } else if (char === '%') {
                         newState.operator = null;
-                        newState.store = 0;
+                        newState.store = null;
                         if (!state.store) {
                             newState.entry = state.entry;
                         } else {
@@ -119,13 +221,13 @@ export class Calculator extends React.Component<IDefaultApplicationWindowProps, 
                     }
                 } else if (memory.includes(char)) {
                     if (char === 'MC') {
-                        newState.memory = 0;
+                        newState.memory = null;
                     } else if (char === 'MR') {
                         newState.entry = state.memory;
                     } else if (char === 'MS') {
-                        newState.memory = state.entry;
+                        newState.memory = trimNr(state.entry);
                     } else if (char === 'M+') {
-                        newState.memory = state.memory * 2;
+                        newState.memory = (Number(state.memory) * 2).toString();
                     }
                 }
 
@@ -155,7 +257,8 @@ export class Calculator extends React.Component<IDefaultApplicationWindowProps, 
                         <Grid item xs>
                             <div className={classes.inputContainer}>
                                 <div className={classes.store}>{this.state.store || null} {this.state.operator}</div>
-                                <input disabled value={this.state.entry.toString()}/>
+                                <input disabled value={this.state.entry.toString()}
+                                       style={this.state.entry.length > 21 ? { fontSize: '14px' } : {}}/>
                             </div>
                         </Grid>
                     </Grid>
