5d1ce6bd63761925e1a4414e3849f512f6c708a5
eclipticrick <eclipticrick@gmail.com>
2018-12-25 01:38:42 +0100

    - added instanceId and selected prop to IWindowInstance

--- src/appdata/window.tsx
+++ src/appdata/window.tsx
@@ -1,7 +1,9 @@
 import {ApplicationId} from './applications';
 
 export interface IWindowInstance {
+    instanceId: number
     applicationId: ApplicationId
     maximized: boolean
     minimized: boolean
+    selected: boolean
 }
