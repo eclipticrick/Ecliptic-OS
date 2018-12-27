8f52c849284b7fb38088e55192276b915bc11138
eclipticrick <eclipticrick@gmail.com>
2018-12-27 16:34:16 +0100

    - added a live clock in the system tray

--- src/components/TaskBar/SystemTray/SystemTray.tsx
+++ src/components/TaskBar/SystemTray/SystemTray.tsx
@@ -1,18 +1,53 @@
 import * as React from 'react';
 import * as classes from './SystemTray.module.scss';
 
-export interface ISystemTrayProps {
-    temp?: any
+export interface ISystemTrayState {
+    time: Date
 }
 
-const systemTray = (props: ISystemTrayProps) => {
-    // const { temp } = props;
+class SystemTray extends React.Component<{}, ISystemTrayState> {
+    public state = {
+        time: new Date()
+    };
 
-    return (
-        <div className={classes.root}>
-            SystemTray
-        </div>
-    );
-};
+    private intervalID: any;
 
-export default systemTray;
+    public componentDidMount() {
+        this.intervalID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    public componentWillUnmount() {
+        clearInterval(this.intervalID);
+    }
+
+    public render() {
+        return (
+            <div className={classes.root}>
+                <div className={classes.clock}>
+                    {this.clockDisplay(this.state.time)}
+                </div>
+            </div>
+        );
+    }
+
+    private tick() {
+        this.setState({
+            time: new Date()
+        });
+    }
+    private clockDisplay(date: Date) {
+        const makeTwoDigits = (nr: number) => {
+            return nr.toString().length === 1 ? '0' + nr : nr
+        };
+        const HH = makeTwoDigits(date.getHours());
+        const MM = makeTwoDigits(date.getMinutes());
+        const SS = makeTwoDigits(date.getSeconds());
+
+        return `${HH}:${MM}:${SS}`
+    }
+}
+
+export default SystemTray;
