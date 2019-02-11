f066f6bb0bb0ab1647b6478d278140d2822b5ab8
eclipticrick <eclipticrick@gmail.com>
2019-02-11 14:27:33 +0100

    removed external dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -153,15 +153,6 @@ module.exports = {
             filename: devMode ? '[name].css' : '[name].[hash].css',
             chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
         })
-    ],
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM'
-    }
+    ]
 
 };
